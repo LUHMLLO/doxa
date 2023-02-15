036f58479ff15c71d91e5d85fc8bd1abcf86065e
luhmllo <luhmllo@pop-os.localdomain>
2023-02-15 15:33:41 -0400

    working on goland crud for the mysql database

--- doxatec/mygosql/main.go
+++ doxatec/mygosql/main.go
@@ -8,81 +8,111 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	username string = "Nevera"
+	password string = "0144250809JDR@f"
+	hostname string = "142.93.207.120"
+	dbname   string = "Smart_Nevera"
+)
+
+var (
+	DSN = fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbname)
+)
+
 type Device struct {
-	Fecha          string
-	DeviceName     string
-	TempSuperior   float64
-	TempIntermedia float64
-	TempInferior   float64
-	Usuario        string
+	Name    string
+	Owner   string
+	TempSup float64
+	TempMid float64
+	TempSub float64
+	Created string
 }
 
-func listAllDevices(db *sql.DB) {
+func CheckConnection() {
+	db, err := sql.Open("mysql", "")
+	if err != nil {
+		fmt.Printf("Error %s when opening DB\n", err)
+		panic(err.Error())
+	}
+	defer db.Close()
+}
 
-	res, err := db.Query("SELECT * FROM TempReg")
+func CreateDatabase(db *sql.DB) {
+	res, err := db.Exec("CREATE DATABASE IF NOT EXISTS " + dbname)
+	if err != nil {
+		fmt.Printf("Error %s when creating DB\n", err.Error())
+		return
+	}
+
+	number, err := res.RowsAffected()
+	if err != nil {
+		fmt.Printf("Error %s when fetching rows\n", err.Error())
+		return
+	}
+
+	fmt.Printf("rows affected %d\n", number)
+	db.Close()
+}
+
+func Connect(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("error verifying connection with db.Ping()")
+		panic(err.Error())
+	}
+	fmt.Println("Successful connection to database \n ")
+}
+
+func CreateTable(db *sql.DB) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS Devices (Name VARCHAR(250), Owner VARCHAR(250), TempSup FLOAT, TempMid FLOAT, TempSub FLOAT, Created TIMESTAMP)")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func InsertToDevices(db *sql.DB) {
+	_, err := db.Exec("INSERT INTO Devices (`Name`,`Owner`,`TempSup`, `TempMid`, `TempSub`, `Created`) VALUES('Nevera 0003','DevClient', 13.5, 8.6, 2.1, NOW())")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ListAllDevices(db *sql.DB) {
+
+	res, err := db.Query("SELECT * FROM Devices")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer res.Close()
 
 	for res.Next() {
-		var fridge Device
+		var column Device
 
-		err := res.Scan(&fridge.Fecha, &fridge.DeviceName, &fridge.TempSuperior, &fridge.TempIntermedia, &fridge.TempInferior, &fridge.Usuario)
+		err := res.Scan(&column.Name, &column.Owner, &column.TempSup, &column.TempMid, &column.TempSub, &column.Created)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 
-		fmt.Println("Fridge:")
-		fmt.Println("- name    : ", fridge.DeviceName)
-		fmt.Println("- owner   : ", fridge.Usuario)
-		fmt.Println("- tempSup : ", fridge.TempSuperior)
-		fmt.Println("- tempMid : ", fridge.TempIntermedia)
-		fmt.Println("- tempSub : ", fridge.TempInferior)
-		fmt.Println("- created : ", fridge.Fecha)
+		fmt.Println("Device")
+		fmt.Println("- name    : ", column.Name)
+		fmt.Println("- owner   : ", column.Owner)
+		fmt.Println("- tempSup : ", column.TempSup)
+		fmt.Println("- tempMid : ", column.TempMid)
+		fmt.Println("- tempSub : ", column.TempSub)
+		fmt.Println("- created : ", column.Created)
 		fmt.Println("")
-
 	}
-
-}
-
-func addNewDevice(db *sql.DB) {
-
-	insert, err := db.Query("INSERT INTO TempReg (`DeviceName`, `TempSuperior`, `TempIntermedia`, `TempInferior`, `Usuario`) VALUES('DevClient - Smart Fridge #03', 9, 6, 3, 'goDev')")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer insert.Close()
 
 }
 
 func main() {
-	// D.S.N (Data Source Name) format
-	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
-	var (
-		host     = "142.93.207.120"
-		database = "Smart_Nevera"
-		user     = "Nevera"
-		password = "0144250809JDR@f"
-		dbPath   = fmt.Sprintf("%s:%s@tcp(%s)/%s", user, password, host, database)
-	)
+	CheckConnection()
 
-	db, err := sql.Open("mysql", dbPath)
-	if err != nil {
-		fmt.Println("error validating sql.Open arguments, dbPath might be incorrect")
-		panic(err.Error())
-	}
-	defer db.Close()
+	db, _ := sql.Open("mysql", DSN)
+	defer CreateDatabase(db)
 
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("error verifying connection with db.Ping()")
-		panic(err.Error())
-	}
-	fmt.Println("Successful connection to database \n ")
-
-	// call functions here
-	//addNewDevice(db)
-	listAllDevices(db)
-
+	Connect(db)
+	CreateTable(db)
+	//InsertToDevices(db)
+	ListAllDevices(db)
 }
