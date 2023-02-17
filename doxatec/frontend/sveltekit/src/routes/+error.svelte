a4777da08b139ca53d2f3e552479f1830006a078
luhmllo <luhmllo@pop-os.localdomain>
2023-02-17 16:33:13 -0400

    better error route layout

--- doxatec/frontend/sveltekit/src/routes/+error.svelte
+++ doxatec/frontend/sveltekit/src/routes/+error.svelte
@@ -2,16 +2,22 @@
     import { page } from "$app/stores";
 </script>
 
-<div>
-    <h1>It seems there has been an error, sorry about that.</h1>
-    {#if $page?.error}
-        <div class="mt-4 p-4 border-y-2">
-            {#if $page?.status}
-                <p>Page status: {$page?.status}</p>
-            {/if}
-            {#if $page?.error?.message}
-                <p>Error message: {$page?.error?.message}</p>
-            {/if}
-        </div>
-    {/if}
-</div>
+<grid class="grid h-min--100vh">
+    <column class="none md:block zindex--1 bg-accent h--100" />
+    <column class="flex justify--center align--center dpt-secondary zindex--2">
+        <section class="w--100">
+            <container class="container block">
+                <h4>Parece ser que hubo un error con tu busqueda</h4>
+                {#if $page?.error}
+                    {#if $page?.status}
+                        <p>Page status: {$page?.status}</p>
+                    {/if}
+                    {#if $page?.error?.message}
+                        <p>Error message: {$page?.error?.message}</p>
+                    {/if}
+                {/if}
+            </container>
+        </section>
+    </column>
+    <column class="none md:block zindex--1 bg-accent h--100" />
+</grid>
