3212cccc8f39642af0bc15a696d7301979a293a8
LUHMLLO <luhmllo06@protonmail.com>
2023-03-14 17:22:54 -0400

    added adapter for render

--- doxatec/frontend/current/svelte.config.js
+++ doxatec/frontend/current/svelte.config.js
@@ -1,15 +1,34 @@
-import adapter from '@sveltejs/adapter-auto';
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: node(),
+
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
 export default config;
+
+// import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
