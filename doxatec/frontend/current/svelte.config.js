feb8936ee803d158c3bfbc9031161b7c763b9c5d
LUHMLLO <luhmllo06@protonmail.com>
2023-03-14 17:46:39 -0400

    back to vercel

--- doxatec/frontend/current/svelte.config.js
+++ doxatec/frontend/current/svelte.config.js
@@ -1,34 +1,15 @@
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: node(),
-
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter()
 	}
 };
 
 export default config;
-
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-// 	// for more information about preprocessors
-// 	preprocess: vitePreprocess(),
-
-// 	kit: {
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
