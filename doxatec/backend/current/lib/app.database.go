3feab5ac54cae9f063c942eccdd7e60b233ac254
LUHMLLO <luhmllo06@protonmail.com>
2023-03-15 18:04:29 -0400

    using old method

--- doxatec/backend/current/lib/app.database.go
+++ doxatec/backend/current/lib/app.database.go
@@ -2,7 +2,6 @@ package lib
 
 import (
 	"database/sql"
-	"fmt"
 
 	_ "github.com/lib/pq"
 )
@@ -12,18 +11,18 @@ type Database struct {
 }
 
 func NewDatabase() (*Database, error) {
-	const (
-		db_user     string = "doxadmin"
-		db_password string = "d@x@dm1n"
-		db_host     string = "localhost"
-		db_port     string = "5432"
-		db_database string = "doxatec"
-	)
+	// const (
+	// 	db_user     string = "doxadmin"
+	// 	db_password string = "d@x@dm1n"
+	// 	db_host     string = "142.93.207.120"
+	// 	db_port     string = "5432"
+	// 	db_database string = "doxatec"
+	// )
 
-	//url_local := "dbname=doxatec user=doxadmin password=d@x@dm1n sslmode=disable"
-	url_public := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", db_user, db_password, db_host, db_port, db_database)
+	url_local := "dbname=doxatec user=doxadmin password=d@x@dm1n sslmode=disable"
+	//url_public := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", db_user, db_password, db_host, db_port, db_database)
 
-	db, err := sql.Open("postgres", url_public)
+	db, err := sql.Open("postgres", url_local)
 	if err != nil {
 		return nil, err
 	}
