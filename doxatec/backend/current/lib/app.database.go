c35884974fd37a1f7793f27122856c57b60e34af
LUHMLLO <luhmllo06@protonmail.com>
2023-03-16 01:32:22 -0400

    attempt to use internal docker db

--- doxatec/backend/current/lib/app.database.go
+++ doxatec/backend/current/lib/app.database.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
@@ -11,18 +12,18 @@ type Database struct {
 }
 
 func NewDatabase() (*Database, error) {
-	// const (
-	// 	db_user     string = "doxadmin"
-	// 	db_password string = "d@x@dm1n"
-	// 	db_host     string = "142.93.207.120"
-	// 	db_port     string = "5432"
-	// 	db_database string = "doxatec"
-	// )
+	const (
+		db_user     string = "doxadmin"
+		db_password string = "d@x@dm1n"
+		db_host     string = "172.17.0.1" //"142.93.207.120"
+		db_port     string = "5432"
+		db_database string = "doxatec"
+	)
 
-	url_local := "dbname=doxatec user=doxadmin password=d@x@dm1n sslmode=disable"
-	//url_public := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", db_user, db_password, db_host, db_port, db_database)
+	//url_local := "dbname=doxatec user=doxadmin password=d@x@dm1n sslmode=disable"
+	url_public := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", db_user, db_password, db_host, db_port, db_database)
 
-	db, err := sql.Open("postgres", url_local)
+	db, err := sql.Open("postgres", url_public)
 	if err != nil {
 		return nil, err
 	}
